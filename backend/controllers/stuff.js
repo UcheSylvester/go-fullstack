cbbea791d396ff83c6a7bb5cd6ee4ecc2fb1a131
UcheSylvester <okorocode@gmail.com>
2019-11-02 21:25:33 +0100

    full stack app done!

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,16 +1,20 @@
 const Thing = require('../models/thing')
+const fs = require('fs')
 
 exports.createThing = (req, res, next) => {
   const url = req.protocol + '://' + req.get('host')
   req.body.thing = JSON.parse(req.body.thing)
 
+  // Giving the thing to be saved a structure using our model
   const thing = new Thing({
     title: req.body.thing.title,
     description: req.body.thing.description,
-    imageUrl: url + 'images/' + req.file.filename,
+    imageUrl: url + '/images/' + req.file.filename,
     price: req.body.thing.price,
     userId: req.body.thing.userId
   })
+
+  // Saving the thing to the database
   thing.save().then(
     () => {
       console.log('thing saved succesfully')
@@ -20,6 +24,7 @@ exports.createThing = (req, res, next) => {
 }
 
 exports.getOneThing = (req, res, next) => {
+  // Getting a single thing from the database
   Thing.findOne({ _id: req.params.id }).then(
     (thing) => res.status(200).json(thing)
   ).catch(
@@ -28,40 +33,38 @@ exports.getOneThing = (req, res, next) => {
 }
 
 exports.modifyThing = (req, res, next) => {
+  /***
+   * MODIFYING A THING
+   * 
+   * Checking if thing contains an uploaded file(image)
+   * 
+   */
 
-  // if (req.file) {
-  //   const url = req.protocol + '://' + req.get('host')
-  //   req.body.thing = JSON.parse(req.body.thing)
+  let thing = new Thing({ _id: req.params._id })
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host')
+    req.body.thing = JSON.parse(req.body.thing)
+    thing = {
+      _id: req.params.id,
+      title: req.body.thing.title,
+      description: req.body.thing.description,
+      imageUrl: url + '/images/' + req.file.filename,
+      price: req.body.thing.price,
+      userId: req.body.thing.userId
+    }
 
-  //   const thing = new Thing({
-  //     title: req.body.thing.title,
-  //     description: req.body.thing.description,
-  //     imageUrl: url + 'images/' + req.file.filename,
-  //     price: req.body.thing.price,
-  //     userId: req.body.thing.userId
-  //   })
+  } else {
+    thing = {
+      _id: req.params.id,
+      title: req.body.title,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl,
+      price: req.body.price,
+      userId: req.body.userId
+    }
+  }
 
-  // } else {
-  //   const newThing = new Thing({
-  //     _id: req.params.id,
-  //     title: req.body.title,
-  //     description: req.body.description,
-  //     imageUrl: req.body.imageUrl,
-  //     price: req.body.price,
-  //     userId: req.body.userId
-  //   })
-  // }
-
-  const newThing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  })
-
-  Thing.updateOne({ _id: req.params.id }, newThing).then(
+  Thing.updateOne({ _id: req.params.id }, thing).then(
     () => res.status(201).json({ message: 'updated successfully' })
   ).catch(
     (error) => res.status(400).json({ error: error })
@@ -69,11 +72,27 @@ exports.modifyThing = (req, res, next) => {
 }
 
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id }).then(
-    () => res.status(200).json({ message: 'deleted successfully' })
-  ).catch(
-    (error) => res.status(400).json({ error: error })
+  // finding a thing in the database to be deleted
+  // Deleting the images(files) from the image folder befor deleting
+  Thing.findOne({ _id: req.params.id }).then(
+    (thing) => {
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        
+        Thing.deleteOne({ _id: req.params.id }).then(
+          () => res.status(200).json({ message: 'deleted successfully' })
+        ).catch(
+          (error) => res.status(400).json({ error: error })
+        )
+      })
+    }
   )
+
+  // Thing.deleteOne({ _id: req.params.id }).then(
+  //   () => res.status(200).json({ message: 'deleted successfully' })
+  // ).catch(
+  //   (error) => res.status(400).json({ error: error })
+  // )
 }
 
 exports.getAllThings = (req, res, next) => {
